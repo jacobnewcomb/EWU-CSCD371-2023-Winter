00e26a455ee0a7466ef61399c4292aeffc1990a3
Jessica Ho <Jschobui@gmail.com>
2023-01-30 22:41:56 -0800

    Unit test

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,10 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace CanHazFunny.Tests
 {
     [TestClass]
     public class JesterTests
     {
-        
+        [TestMethod]
+        public void TestTellJoke
+        {
+            JokeService jokeServiceTest = new();
+            string joke = jokeServiceTest.TellJoke();
+            Assert.NotNull(joke);
+        }
     }
 }
