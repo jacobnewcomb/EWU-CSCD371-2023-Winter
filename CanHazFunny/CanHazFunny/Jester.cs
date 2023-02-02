4caea6981f8dbff2e24a87a9fc4d1f1de9bb1b34
Jacob Newcomb <75453587+jacobnewcomb@users.noreply.github.com>
2023-02-02 14:12:01 -0800

    Update CanHazFunny/CanHazFunny/Jester.cs
    Co-authored-by: Hunter T. <strangeranger01@randomserver.xyz>

--- CanHazFunny/CanHazFunny/Jester.cs
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -9,7 +9,10 @@ namespace CanHazFunny
 {
     public class Jester
     {
-        public JokeService JokeService { get => _jokeService; set => _jokeService = value ?? throw new ArgumentNullException(value.ToString()); }
+        public IService JokeService { 
+            get => _jokeService; 
+            set => _jokeService = value ?? throw new ArgumentNullException(value.ToString());
+        }
         private JokeService? _jokeService;
         public JokeServiceWriter JokeServiceWriter { get => _jokeServiceWriter; set => _jokeServiceWriter = value ?? throw new ArgumentNullException(value.ToString()); }
         private IServiceWriter? _JokeServiceWriter;
