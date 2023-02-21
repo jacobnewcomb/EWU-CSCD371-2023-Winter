a054b8be3cc8c2c31357709a7310aa553ffaeb84
Jessica Ho <Jschobui@gmail.com>
2023-02-20 23:25:50 -0800

    Program file

---
+++ Calculate/Calculate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Calculate
+{
+	public class Program
+	{
+		public Action<string> WriteLine { get; init; } = Console.WriteLine;
+		public Func<string> ReadLine { get; init; } = Console.ReadLine;
+
+		public Program(Action<string> writeLine, Func<string> readLine)
+		{
+			WriteLine = writeLine;
+			ReadLine = readLine;
+		}
+
+		public Program() { } //empty constr
+
+		static void Main(string[] args)
+		{
+			var program = new Program();
+			{
+				program.WriteLine("WriteLine test");
+				string readInput = program.ReadLine();
+
+				program.WriteLine($"Test ReadLine {readInput}");
+			}
+		}
+
+	}
+}
+
