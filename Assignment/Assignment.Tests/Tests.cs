455c3712d5e54c2c30b105137aaa8938ac2ca6da
Jessica Ho <Jschobui@gmail.com>
2023-03-09 22:18:34 -0800

    additional tests to check correct csvrow

--- Assignment/Assignment.Tests/Tests.cs
+++ Assignment/Assignment.Tests/Tests.cs
@@ -15,6 +15,30 @@ namespace Assignment.Tests
                 }
             }
         }
+
+        [TestMethod]
+        public void CsvRows_ReturnsExpectedRows()
+        {
+            // Arrange
+            SampleDataTest sample = new SampleDataTest();
+            string[] expectedRows = new string[]
+            {
+                "1,Priscilla,Jenyns,pjenyns0@state.gov,3205 S University Rd #73,Spokane,WA,99206",
+                "2,Karin,Joder,kjoder1@quantcast.com,3214 E Congress Ave,Spokane,WA,99223",
+                "3,Chadd,Stennine,cstennine2@wired.com,3315 W Queen Pl,Spokane,WA,99205",
+                "4,Fremont,Pallaske,fpallaske3@umich.edu,3317 E Jackson Ave,Spokane,WA,99217",
+                "5,Melisa,Kerslake,mkerslake4@dion.ne.jp,3324 S Arthur St,Spokane,WA,99203",
+                "6,Darline,Brauner,dbrauner5@biglobe.ne.jp,3406 E Empire Ave,Spokane,WA,99217",
+                "7,Issiah,Bester,ibester6@psu.edu,2811 S Fiske St,Spokane,WA,99223",
+                "8,Joly,Scneider,jscneider7@pagesperso-orange.fr,2833 W Beacon Ave,Spokane,WA,99208",
+                "9,Swen,Mulvany,smulvany8@surveymonkey.com,3016 E 5th Ave,Spokane,WA,99202",
+                "10,Scarface,Dennington,sdennington9@chron.com,3711 E 32nd Ave,Spokane,WA,99223"
+            };
+            IEnumerable<string> actualRows = sample.CsvRows;
+
+            CollectionAssert.AreEqual(expectedRows, actualRows.ToArray());
+        }
+
         [TestMethod]
         public void GetUniqueSortedListOfStatesGivenCsvRows_Hardcoded_Works()
         {
